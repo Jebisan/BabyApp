e9b85cb6574182709d01edcd9e66e9a061696631
Jebisan H. Nadarajah <Jebisan.h.n@hotmail.com>
2021-02-11 20:14:37 +0100

    Clean up in Find mapview

--- screens/Find/MapView/MapView.js
+++ screens/Find/MapView/MapView.js
@@ -1,24 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, View, SafeAreaView} from "react-native";
-import {useSelector} from 'react-redux';
+import { StyleSheet, View} from "react-native";
 import MapContainer from '../../../components/MapContainer';
 
  const Find = (props) => {
-  const allGroups = useSelector(state => state.allGroups)
-  const allUsers = useSelector(state => state.allUsers)
-
   const [searchText, setSearchText] = useState('');
-  const [selectedIndex, setSelectedIndex] = useState(0);
-  const [filteredUsers, setFilteredUsers] = useState([])
-  const [filteredGroups, setFilteredGroups] = useState([])
-  const [cityData, setCityData] = useState([])
-  const [hideResults, setHideResults] = useState(false);
   const [coordinates, setCoordinates] = useState(undefined);
 
 
-
 useEffect(() => {
-  setHideResults(false);
   if(searchText) {
     const tempPostnummerData = []
     fetch(`https://dawa.aws.dk/postnumre/autocomplete?q=${searchText}`).then((response) => {
@@ -40,56 +29,15 @@ useEffect(() => {
     });
   } 
   else {
-    setCityData([]);
   }
 }, [searchText])
 
 
-const renderResults = city => {
-    if(selectedIndex==0){
-      let tempUserArray = [] 
-      allUsers.forEach(user => {
-        if(user.city.trim().includes(city.navn.trim())){
-          tempUserArray.push(user);
-        } else {
-        }
-      });
-      setFilteredUsers(tempUserArray)
-    }
-    
-    if(selectedIndex==1){
-      let tempGroupArray = [] 
-      allGroups.forEach(group => {
-        if(group.city.includes(city.navn.trim())){
-          tempGroupArray.push(group);
-        }
-      });
-      setFilteredGroups(tempGroupArray);
-    }
-
-    if(selectedIndex==2){
-      const coordinates = {
-        latitude: city.visueltcenter_y,
-        longitude: city.visueltcenter_x,
-        latitudeDelta: 7.0922,
-        longitudeDelta: 7.0421
-      }
-      setCoordinates(coordinates);
-    }  
-}
-  
-  setSelectedCityHandler = (item) => {
-    renderResults(item);
-    setHideResults(true);
-    setSearchText('');
-  }
-
-
     return (
-<View style= {styles.parentContainer} >
-  <MapContainer coordinates = {coordinates} navigation = {props.navigation} />
-</View>
-)}
+      <View style= {styles.parentContainer} >
+        <MapContainer coordinates = {coordinates} navigation = {props.navigation} />
+      </View>
+    )}
 
 const styles = StyleSheet.create({
   parentContainer: {
